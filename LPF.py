8d298171d0c4b278bcbbc1dceec1399c18101b35
Emmanuel Jesus Estallo <erstallo@up.edu.ph>
2021-05-21 01:29:22 +0800

    Create LPF.py

---
+++ LPF.py
@@ -0,0 +1,19 @@
+import subprocess
+
+#location of XVIIx64.exe
+prog_path = 'D:\LTSpice'
+
+txt_file = 'LPF.txt'
+Res1_orig = 'Res1=1k'
+Cap1_orig = 'Cap1=0.1u'
+
+for i in range(2):
+    with open(txt_file, 'rb') as file:
+        data_orig = file.read()
+        data_temp = data_orig.replace(Res1_orig.encode('ascii'), 'Res1=10k'.encode('ascii'))\
+            .replace(Cap1_orig.encode('ascii'), 'Cap1=1u'.encode('ascii'))
+
+    with open(f'new_file.txt{i}', 'wb') as file:
+        file.write(data_temp)
+
+    subprocess.call(prog_path +f'\XVIIx64.exe -b new_file.txt-{i}')
